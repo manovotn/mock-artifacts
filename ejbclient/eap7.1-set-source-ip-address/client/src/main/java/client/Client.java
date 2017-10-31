be074a33d6b13b4426f66660cf011766e2943e71
Matej Novotny <manovotn@redhat.com>
2017-10-31 12:29:41 +0100

    Test this

--- ejbclient/eap7.1-set-source-ip-address/client/src/main/java/client/Client.java
+++ ejbclient/eap7.1-set-source-ip-address/client/src/main/java/client/Client.java
@@ -1,10 +1,12 @@
 package client;
 
 import java.util.Properties;
+
 import javax.naming.Context;
 import javax.naming.InitialContext;
 
 import org.wildfly.naming.client.WildFlyInitialContextFactory;
+import org.xnio.XnioWorker;
 
 import ejb.HelloBeanRemote;
 
@@ -12,7 +14,14 @@ public class Client {
 
     public static void main(String[] args)
             throws Exception {
+        
+        System.setProperty("wildfly.config.url", ClassLoader.getSystemResource(
+            "client/test.xml").toString());
         InitialContext ctx = new InitialContext(getCtxProperties());
+        if (XnioWorker.getContextManager().getGlobalDefault().getIoThreadCount() == 99) {
+            // this is set in test.xml and SHOULD THROW EXCEPTION
+            throw new IllegalStateException();
+        }
         try {
             String lookupName = "ejb:/server/HelloBean!ejb.HelloBeanRemote?stateful";
             HelloBeanRemote bean = (HelloBeanRemote)ctx.lookup(lookupName);
